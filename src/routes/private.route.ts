7f9442826bfd8c03e8b8a8abc8e9b924b44f8893
Edi Ruhiat <edlr@windowslive.com>
2022-03-17 18:41:22 +0700

    🕵🏽Add token middleware to private routes

--- src/routes/private.route.ts
+++ src/routes/private.route.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from "express";
+import Verify from "../middlewares/verify_token";
 
 export const privateRoutes = express.Router();
 
-privateRoutes.get("/", (req: Request, res: Response) => {
+privateRoutes.get("/", Verify, (req: Request, res: Response) => {
   return res.status(200).send({
     status: true,
     message: "this is private route.",
