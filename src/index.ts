f23fbdc2c256503e9d0fb5b2557b8e778f07718b
Edi Ruhiat <edlr@windowslive.com>
2022-03-17 18:39:34 +0700

    "🏛 Add private routes to index"

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import { authRoutes } from "./routes/auth.route";
+import { privateRoutes } from "./routes/private.route";
 
 const app = express();
 
 // middlewares
 app.use(express.json());
 
-// routes middleware
+// routes
 app.use("/api/user", authRoutes);
+app.use("/api/private", privateRoutes);
 
 app.listen(process.env.APP_PORT, () => {
   console.log(
